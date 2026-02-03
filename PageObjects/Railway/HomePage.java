99f217f0440c062a6e592ee1feecde785037b99c
Vanthiphuongthao <vanthiphuongthao30@gmail.com>
2026-02-03 17:26:57 +0700

    First Commit

---
+++ PageObjects/Railway/HomePage.java
@@ -0,0 +1,14 @@
+package Railway;
+
+import Constant.Constant;
+
+public class HomePage extends GeneralPage{
+	//Locators
+	//Elements
+	//Methods
+	public HomePage open() {
+		Constant.WEBDRIVER.navigate().to(Constant.RAILWAY_URL);
+		return this;
+	}
+
+}
