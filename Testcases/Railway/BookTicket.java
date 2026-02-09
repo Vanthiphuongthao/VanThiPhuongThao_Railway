8785e66a3ad3afb708bd3fea4e90dbc0a51c460f
Vanthiphuongthao <vanthiphuongthao30@gmail.com>
2026-02-10 05:35:10 +0700

    Refine BookTicket test logic and assertions

--- Testcases/Railway/BookTicket.java
+++ Testcases/Railway/BookTicket.java
@@ -17,12 +17,12 @@ public class BookTicket extends BaseTest {
 	@Test
 	public void TC12() {
 		UserAccount userAccount = new UserAccount(Constant.USERNAME, Constant.PASSWORD, Constant.PID);
-		
+
 		int targetDate = 2;
 		String departFrom = Station.NHA_TRANG.getStation();
 		String arriveAt = Station.HUE.getStation();
 		String seatType = SeatType.SOFT_BED_AC.getSeatType();
-		String amountTicket = "1";		
+		String amountTicket = "1";
 
 		System.out.println("TestCase12 - User can't create account with an already in-use email");
 		System.out.println("Pre-condition: an actived account is existing");
@@ -34,29 +34,78 @@ public class BookTicket extends BaseTest {
 		System.out.println("2. Login with a valid account");
 		LoginPage loginPage = homePage.gotoPage(Tabs.LOGIN, LoginPage.class);
 		loginPage.login(userAccount, HomePage.class);
-		
+
 		System.out.println("3. Click on \"Book ticket\" tab");
+		BookTicketPage bookTicketPage = homePage.gotoPage(Tabs.BOOK_TICKET, BookTicketPage.class);
+
 		System.out.println("4. Select the next 2 days from \"Depart date\"");
 		System.out.println("5. Select Depart from \"Nha Trang\" and Arrive at \"Huế\"");
 		System.out.println("6. Select \"Soft bed with air conditioner\" for \"Seat type\"");
 		System.out.println("7. Select \"1\" for \"Ticket amount\"");
 		System.out.println("8. Click on \"Book ticket\" button");
-		
-		BookTicketPage bookTicketPage = homePage.gotoPage(Tabs.BOOK_TICKET, BookTicketPage.class);
-		
+
 		bookTicketPage.bookTicket(targetDate, departFrom, arriveAt, seatType, amountTicket);
-		
+
 		// verify book ticket message
 		String actualSuccessMsg = bookTicketPage.getBookTicketSuccessMsgText();
 		String expectedSuccessMsg = Message.BOOK_TICKET_SUCCESS_MSG.getMessage();
 		String errorMsg = Message.BOOK_TICKET_MSG_NOT_DISPLAYED.getMessage();
-		
+
 		Assert.assertEquals(actualSuccessMsg, expectedSuccessMsg, errorMsg);
-		
-		// verify depart station
-		
+
+		// verify Ticket information
+
 		Map<String, String> ticketInfo = bookTicketPage.getTicketInfo(2);
-		
+
+		Assert.assertEquals(ticketInfo.get(TicketHeader.DEPART_STATION.getHeaderText()), departFrom);
+		Assert.assertEquals(ticketInfo.get(TicketHeader.ARRIVE_STATION.getHeaderText()), arriveAt);
+		Assert.assertEquals(ticketInfo.get(TicketHeader.SEAT_TYPE.getHeaderText()), seatType);
+		Assert.assertEquals(ticketInfo.get(TicketHeader.AMOUNT.getHeaderText()), amountTicket);
+	}
+
+	@Test
+	public void TC13() {
+		UserAccount userAccount = new UserAccount(Constant.USERNAME, Constant.PASSWORD, Constant.PID);
+
+		int targetDate = 25;
+		String departFrom = Station.NHA_TRANG.getStation();
+		String arriveAt = Station.SAI_GON.getStation();
+		String seatType = SeatType.SOFT_SEAT_AC.getSeatType();
+		String amountTicket = "5";
+
+		System.out.println("TestCase13 - User can book many tickets at a time");
+		System.out.println("Pre-condition: an actived account is existing");
+
+		System.out.println("1. Navigate to QA Railway Website");
+		HomePage homePage = new HomePage();
+		homePage.open();
+
+		System.out.println("2. Login with a valid account");
+		LoginPage loginPage = homePage.gotoPage(Tabs.LOGIN, LoginPage.class);
+		loginPage.login(userAccount, HomePage.class);
+
+		System.out.println("3. Click on \"Book ticket\" tab");
+		BookTicketPage bookTicketPage = homePage.gotoPage(Tabs.BOOK_TICKET, BookTicketPage.class);
+
+		System.out.println("4. Select the next 25 days from \"Depart date\"");
+		System.out.println("5. Select \"Nha Trang\" for \"Depart from\" and \"Sài Gòn\" for \"Arrive at\"");
+		System.out.println("6. Select \"Soft seat with air conditioner\" for \"Seat type\"");
+		System.out.println("7. Select \"5\" for \"Ticket amount\"");
+		System.out.println("8. Click on \"Book ticket\" button");
+
+		bookTicketPage.bookTicket(targetDate, departFrom, arriveAt, seatType, amountTicket);
+
+		// verify book ticket message
+		String actualSuccessMsg = bookTicketPage.getBookTicketSuccessMsgText();
+		String expectedSuccessMsg = Message.BOOK_TICKET_SUCCESS_MSG.getMessage();
+		String errorMsg = Message.BOOK_TICKET_MSG_NOT_DISPLAYED.getMessage();
+
+		Assert.assertEquals(actualSuccessMsg, expectedSuccessMsg, errorMsg);
+
+		// verify Ticket information
+
+		Map<String, String> ticketInfo = bookTicketPage.getTicketInfo(2);
+
 		Assert.assertEquals(ticketInfo.get(TicketHeader.DEPART_STATION.getHeaderText()), departFrom);
 		Assert.assertEquals(ticketInfo.get(TicketHeader.ARRIVE_STATION.getHeaderText()), arriveAt);
 		Assert.assertEquals(ticketInfo.get(TicketHeader.SEAT_TYPE.getHeaderText()), seatType);
@@ -64,11 +113,18 @@ public class BookTicket extends BaseTest {
 	}
 	
 	@Test
-	public void TC13() {
+	public void TC14() {
 		
-		System.out.println("TestCase13 - User can book many tickets at a time");
+		System.out.println("TestCase14 - User can check price of ticket from Timetable");
 		System.out.println("Pre-condition: an actived account is existing");
 		
+		System.out.println("1. Navigate to QA Railway Website");
+		System.out.println("2. Login with a valid account");
+		System.out.println("3. Click on \"Timetable\" tab");
+		System.out.println("4. Click on \"check price\" link of the route from \"Đà Nẵng\" to \"Sài Gòn\"");
+		
 	}
+	
+	
 
 }
