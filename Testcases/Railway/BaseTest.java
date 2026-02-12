385eb001a0c1a0f3cdc5351c2b05dea75d025065
Vanthiphuongthao <vanthiphuongthao30@gmail.com>
2026-02-12 08:21:12 +0700

    Update BaseTest setup and TestNG configuration

--- Testcases/Railway/BaseTest.java
+++ Testcases/Railway/BaseTest.java
@@ -1,16 +1,25 @@
 package Railway;
 
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 
 import Constant.Constant;
 
 public class BaseTest {
-
+	@Parameters({ "browser" })
 	@BeforeMethod
-	public void beforeMethod() {
-		Constant.WEBDRIVER = new ChromeDriver();
+	public void beforeMethod(@Optional("chrome") String myBrowser) {
+		if (myBrowser.equals("chrome")) {
+			Constant.WEBDRIVER = new ChromeDriver();
+
+		} else if (myBrowser.equals("firefox")) {
+			Constant.WEBDRIVER = new FirefoxDriver();
+
+		}
 		Constant.WEBDRIVER.manage().window().maximize();
 	}
 
